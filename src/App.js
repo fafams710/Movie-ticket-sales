9843b74cccdd7a2f71cd00efddef2c530ef58fcf
@Lawrenceio <hanslawrence38@gmail.com>
2025-01-21 16:18:40 +0800

    mga bugoy nangalikot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Footer from './Footer'
 
 function App() {
   const [isLogin, setIsLogin] = useState(true);
@@ -76,6 +77,49 @@ function App() {
   return (
     <div className="min-h-screen bg-black text-white relative">
       <Navbar setIsFormVisible={setIsFormVisible} setIsLogin={setIsLogin} />
+
+      <section class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-12 mx-auto">
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+    <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{
+  backgroundImage: 'url(/12.jpg)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center', 
+}}></div>
+
+    <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+        <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Salome Napahiyaw</h3>
+
+        <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span class="font-bold text-gray-800 dark:text-gray-200">$5 ticket</span>
+            <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+        </div>
+    </div>
+</div>
+
+<div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+    <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{
+  backgroundImage: 'url(/OIP.jpg)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center', 
+}}></div>
+
+    <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+        <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">Bugot kinalikot</h3>
+
+        <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <span class="font-bold text-gray-800 dark:text-gray-200">$5 ticket</span>
+            <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+        </div>
+    </div>
+</div>  
+        </div>
+    </div>
+</section>
+
+
+
 
       {/* Login/SignUp Form */}
       {isFormVisible && !isLoggedIn && (
@@ -213,6 +257,7 @@ function App() {
           </div>
         </div>
       )}
+      <Footer/>
     </div>
   );
 }
