6a7645a2268d41b42e133841b7d02c29fb8fa74d
fafams710 <krparbo@student.hau.edu.ph>
2025-03-23 23:56:37 +0800

    working backend only need frontend

--- backend/tickets/admin.py
+++ backend/tickets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import TicketType
 
-# Register your models here.
+class TicketTypeAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('concert', 'category', 'price', 'total_quantity', 'remaining_quantity')
+    
+    # Fields that can be searched in the admin interface
+    search_fields = ('concert__name', 'category')
+    
+    # Filters to add in the admin interface
+    list_filter = ('category', 'concert')
+
+# Register the TicketType model with the custom admin class
+admin.site.register(TicketType, TicketTypeAdmin)
